4d7df35b474a94ed02eeff8064cf8d8b84cc752b
Clement-Cauet <70692830+Clement-Cauet@users.noreply.github.com>
2025-04-07 12:13:17 +0200

    feat: add profile component with routing and update navbar links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './views/home/home.component';
 import { ScheduleComponent } from './views/schedule/schedule.component';
 import { TripsComponent } from './views/trips/trips.component';
 import { ContactComponent } from './views/contact/contact.component';
+import { ProfileComponent } from './views/profile/profile.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'schedule', component: ScheduleComponent},
     {path: 'contact', component: ContactComponent},
+    {path: 'profile', component: ProfileComponent},
     {path: 'trips/:routeId', component: TripsComponent},
     {path: '**', redirectTo: 'home'},
 ];
